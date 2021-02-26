806e2f889aeae9ec47cf79053cc5add74a302430
Mumuuuu <mumu4ctf@gmail.com>
2021-02-26 08:56:59 +0800

    Edit rsa/read_pem output format

--- crypto/rsa/read_pem_file/read.py
+++ crypto/rsa/read_pem_file/read.py
@@ -6,5 +6,11 @@ f = input("Input the file name : ")
 
 f = importKey(open(f , "r").read())
 
-print(f.n)
-print(f.e)
+print(f"n = {f.n}")
+try:
+    print(f"d = {f.d}")
+    print(f"e = {f.e}")
+except:
+    print(f"e = {f.e}")
+
+
