1b8cfcbd11e2abba195aabfc986721159a94cff5
Mumuuuu <mumu4ctf@gmail.com>
2021-05-03 10:25:36 +0800

    Edit pem_read

--- crypto/rsa/read_pem_file/read.py
+++ crypto/rsa/read_pem_file/read.py
@@ -1,16 +1,23 @@
 #!/usr/bin/python3
 
 from Crypto.PublicKey.RSA import importKey
+from glob import glob
+from sys import argv 
 
-f = input("Input the file name : ")
+if len(argv) == 1:
+    print("Usage : ./read.py <file> <file> ...")
+    exit()
 
-f = importKey(open(f , "r").read())
+for i in argv[1:]:
+    for j in glob(i):
+        f = importKey(open(j , "r").read())
 
-print(f"n = {f.n}")
-try:
-    print(f"d = {f.d}")
-    print(f"e = {f.e}")
-except:
-    print(f"e = {f.e}")
+        print("=" * 5 + " {} ".format(j) + "=" * 5)
+        print(f"n = {f.n}")
+        try:
+            print(f"d = {f.d}")
+            print(f"e = {f.e}")
+        except:
+            print(f"e = {f.e}")
 
 
